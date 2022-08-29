0e3c6da9448f9200baa2961614f78bd7c2f5be7d
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-08-29 00:28:47 -0600

    using middleware from external libraries

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const logger = require("morgan");
+
 // import routes
 const indexRouter = require("./routes/index");
 const itemsRouter = require("./routes/items");
 const categoriesRouter = require("./routes/categories");
 
+const app = express();
+
 // middleware (goes first than routes)
+app.use(logger("dev"));
+app.use(cors()); // enable All CORS Requests
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
 
